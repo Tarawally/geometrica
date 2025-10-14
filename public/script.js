e70f876848e17ebcdd2296a9e2ecdff81f1399b9
Samuel Tarawally <41825140+Tarawally@users.noreply.github.com>
2025-10-14 16:25:25 +0100

    feat(index.html): outline structure for 2D shape rendering

--- public/script.js
+++ public/script.js
@@ -1,7 +1,6 @@
 const canvas = document.getElementById('app-canvas');
 const ctx = canvas.getContext('2d');
 
-// Do these pixel do anything as they don't seem to scale when changed
 canvas.width = 800;
 canvas.height = 600;
 
